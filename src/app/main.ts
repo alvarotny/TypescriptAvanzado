32f963c7ff19721c233a0e408d4d2402433c8fd5
Alvaro Pariona <tapariona@gmail.com>
2025-11-23 21:21:19 -0500

    [ADD] usar Partial para hacer todas las propiedades opcionales en Product

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,4 +1,4 @@
-import {productList, addProduct} from "./products/product.service"
+import {productList, addProduct, updateP} from "./products/product.service"
 import {Producto, Sizes, TAG} from "./products/product.model"
 import {fa, faker}from "@faker-js/faker"
 import { CreateNewProduct } from "./products/product.dto"
@@ -11,39 +11,23 @@ for(let index = 0; index < 5; index++){
   precio: Number(faker.commerce.price({min:10, max:200})),
   medida:faker.helpers.arrayElement<Sizes>(["L","M","S","XL"]),
   tag:TAG.tag1,
-  categoryId: faker.string.uuid()
-
+  categoryId: faker.string.uuid(),
+  list:[]
   }
   addProduct(producto)
 }
 
-// mostrar productos
-const getProduct = ():void =>{
-  productList.forEach((item) =>{
-    console.log(`ID: ${item.id} Producto: ${item.nombre} precio: ${item.precio} medida: ${item.medida}`)
-  })
-}
-getProduct()
-
 //
-// Partial >= toma las propiedades de Producto y los establece como opcionales, util para actualizaciones cortas
-// type CreateNewProduct = Omit<Producto,"id" | "createdAT" | "updateAT" | "cliente">
-type newProductType = Pick<Producto, "nombre" | "cliente"> //crea un nuevo tipo respecto a un producto establecido
-const updateProduct = (id: number, changes: Partial<Producto>) => {
-  const index = productList.findIndex(p => p.id === id);
+  // nombre:string,
+  // decription:string,
+  // color:string,
+  // precio:number,
+  // medida:Sizes,
+  // tag: TAG,
+  // cliente:Client
 
-  if (index === -1) {
-    console.log("Producto no encontrado");
-    return;
-  }
-  // reemplazar data existente
-  Object.assign(productList[index]!, changes)
-};
-
-
-updateProduct(6642163147648024, { precio: 120 });
-
-
-
-// getProduct()
-// console.log(productList)
+// const productOne = productList[0];
+// const rpta  = updateP(productOne?.id,{nombre:"Muebles",description:"Ventas des muebles al por mayor", precio: 234})
+// console.log(rpta)
+// const rpta2 = findProduct({id:productOne?.id,price:1212});
+// console.log(rpta2)
